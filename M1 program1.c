de122701693eacfc976f241d86b7672781792ff9
kevaltrivedii <tkeval1610@gmail.com>
2023-01-23 14:43:37 +0530

    assignment 1 done

---
+++ M1 program1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main (){
+     printf("MY SELF\n\n");
+     printf("My name is keval trivedi.\n\n");
+     printf("My birth date is 16/10/2003.\n\n");
+     printf("I am 19 years old.\n\n");
+     printf("I live in ahmedabad.\n\n");
+}
---
+++ M2 program1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+
+    int n;
+
+    printf("Enter a number :");
+  
+    scanf("%d",&n);
+
+    printf("square of the number %d is %d\n" ,n,n*n);
+
+    printf("Cube of the number %d is %d\n" , n,n*n*n);
+
+
+
+}
