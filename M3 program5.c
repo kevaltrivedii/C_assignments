9ea41ddcfdc6a9853ee04af04379293a300ad74e
kevaltrivedii <tkeval1610@gmail.com>
2023-02-05 12:57:44 +0530

    program completed

---
+++ M3 program5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>    
+void main(){
+    
+ int a=0,b=1,c,i,number;
+ 
+ printf("Enter the number:");  
+ 
+ scanf("%d",&number); 
+ 
+ printf("\n%d %d",a,b);
+ 
+ for(i=2;i<number;++i){
+     
+  c=a+b;
+  
+  printf(" %d",c); 
+  
+  a=b;    
+  b=c;    
+ }  
+
+}
