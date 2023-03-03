fcf2fb6b1ec208af6a4b8504a448bd377fd7f674
kevaltrivedii <tkeval1610@gmail.com>
2023-03-03 11:19:14 +0530

    program completed

---
+++ M3 program16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int isPalindrome(int num, int temp) {
+  
+    if (num == 0) {
+        return temp;
+    }
+   
+    temp = (temp * 10) + (num % 10);
+    return isPalindrome(num / 10, temp);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("You entered: %d\n", num);
+    int temp = isPalindrome(num, 0);
+    if (temp == num) {
+        printf("The number is a palindrome.\n");
+    } else {
+        printf("The number is not a palindrome.\n");
+    }
+    return 0;
+}
+
+
+
+
+//incomoplete//??
